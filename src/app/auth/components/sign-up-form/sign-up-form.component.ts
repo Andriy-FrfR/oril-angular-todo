8b5ef8596dc10022320217f859f8db0464f8cbff
Andriy-FrfR <andriy.lukashewwich@gmail.com>
2022-10-05 22:50:10 +0300

    added validation of form controls

--- src/app/auth/components/sign-up-form/sign-up-form.component.ts
+++ src/app/auth/components/sign-up-form/sign-up-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up-form',
@@ -8,17 +8,55 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class SignUpFormComponent implements OnInit {
   signUpForm: FormGroup = new FormGroup({
-    fullName: new FormControl(null),
-    username: new FormControl(null),
-    email: new FormControl(null),
-    phone: new FormControl(null),
-    address: new FormControl(null),
+    name: new FormControl(null, [Validators.required]),
+    username: new FormControl(null, [Validators.required]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    phone: new FormControl(null, [
+      Validators.required,
+      Validators.pattern('[0-9]{3}-[0-9]{3}-[0-9]{4}'),
+    ]),
+    address: new FormControl(null, [Validators.required]),
     suite: new FormControl(null),
-    city: new FormControl(null),
-    ZIP: new FormControl(null),
+    city: new FormControl(null, [Validators.required]),
+    zipcode: new FormControl(null, [
+      Validators.required,
+      Validators.pattern('[0-9]{5}'),
+    ]),
   });
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  get name() {
+    return this.signUpForm.get('name');
+  }
+
+  get username() {
+    return this.signUpForm.get('username');
+  }
+
+  get email() {
+    return this.signUpForm.get('email');
+  }
+
+  get phone() {
+    return this.signUpForm.get('phone');
+  }
+
+  get address() {
+    return this.signUpForm.get('address');
+  }
+
+  get suite() {
+    return this.signUpForm.get('suite');
+  }
+
+  get city() {
+    return this.signUpForm.get('city');
+  }
+
+  get zipcode() {
+    return this.signUpForm.get('zipcode');
+  }
 }
